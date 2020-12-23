acfea65d7960c9204b9ed19945b5fd2c698cab0c
Clming <kimyongsung0419@gmail.com>
2020-12-23 20:20:34 +0900

    SkyBlue BOT ver 1.1 업데이트
    음악 재생기능 추가
    (Youtube_dl 기반)

--- BOT.py
+++ BOT.py
@@ -1,6 +1,10 @@
 import discord
 import random
 from discord.ext import commands
+from bs4 import BeautifulSoup
+import urllib
+import youtube_dl
+import os
 
 app = commands.Bot(command_prefix = '!')
 app.remove_command('help')
@@ -10,18 +14,80 @@ async def on_ready():
     print('다음으로 로그인합니다:')
     print(app.user.name)
     print('connection was successful')
+    print('----------')
     await app.change_presence(activity = discord.Game(name='개발'))
 
 @app.command()
-async def hello(ctx):
-    await ctx.send('Hello, World!')
+async def play(ctx, url : str):
+    song_there = os.path.isfile("song.mp3")
+    try:
+        if song_there:
+            os.remove("song.mp3")
+    except PermissionError:
+        await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+        return
+
+    voiceChannel = discord.utils.get(ctx.guild.voice_channels, name = 'General')
+    await voiceChannel.connect()
+    voice = discord.utils.get(app.voice_clients, guild=ctx.guild)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    for file in os.listdir("./"):
+        if file.endswith(".mp3"):
+            os.rename(file, "song.mp3")
+    voice.play(discord.FFmpegPCMAudio("song.mp3"))
+
+
+@app.command()
+async def leave(ctx):
+    voice = discord.utils.get(app.voice_clients, guild=ctx.guild)
+    if voice.is_connected():
+        await voice.disconnect()
+    else:
+        await ctx.send("The bot is not connected to a voice channel.")
+
+
+@app.command()
+async def pause(ctx):
+    voice = discord.utils.get(app.voice_clients, guild=ctx.guild)
+    if voice.is_playing():
+        voice.pause()
+    else:
+        await ctx.send("Currently no audio is playing.")
+
+
+@app.command()
+async def resume(ctx):
+    voice = discord.utils.get(app.voice_clients, guild=ctx.guild)
+    if voice.is_paused():
+        voice.resume()
+    else:
+        await ctx.send("The audio is not paused.")
+
+
+@app.command()
+async def stop(ctx):
+    voice = discord.utils.get(app.voice_clients, guild=ctx.guild)
+    voice.stop()
+
 
 @app.command(pass_context = True)
 async def help(ctx):
-    embed = discord.Embed(title = 'Help', description = 'Ver = 1.0' , color = 0xFF5733)
-    embed.add_field(name = "!dice", value = "1~6범위 내의 난수를 알려줍니다" , inline = False)
-    embed.add_field(name = "!repeat (반복할말)", value = "입력된 말을 SkyBlue가 다시 말합니다" , inline = False)
+    embed = discord.Embed(title = 'Help', description = 'Ver = 1.1' , color = 0xFF5733)
+    embed.add_field(name = "!dice", value = "1~6범위 내의 난수를 알려줍니다" , inline = True)
+    embed.add_field(name = "!repeat [반복할말]", value = "입력된 말을 SkyBlue가 다시 말합니다" , inline = False)
     embed.add_field(name = "!hello" , value = "Hello, World!" , inline = False)
+    embed.add_field(name = "!play [유튜브 음악 URL]" , value = "입력받은 유튜브 URL의 영상을 오디오로 변환하며 재생합니다" , inline = False)
+    embed.add_field(name = "!pause !resume !stop" , value = "각각 멈춤, 재생, 종료입니다" , inline = False)
 
     await ctx.send(embed = embed)
 
@@ -36,4 +102,4 @@ async def dice(ctx):
 
 
 
-app.run('Token')                        
+app.run('NzkwNDQ5OTUwMTY1MTcyMjQ0.X-Ax4Q.OIfjnylQ8NYydTyY0Pb_9tuwqE8')  #github 푸시할때 토큰 가리세요      
